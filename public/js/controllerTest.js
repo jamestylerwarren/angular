a79d7761976d2c1aff56bbe22a10aea7098ce7e6
Tyler Warren <warrejt@yahoo.com>
2016-10-29 10:15:42 -0500

    created new object for newItems, new array for items;
    need to get button to add input from page to items;

--- public/js/controllerTest.js
+++ public/js/controllerTest.js
@@ -4,21 +4,25 @@
     var app = angular.module("controllerTest", []);
     app.controller("CartController", function() {
 
-        var items = {
-        	name: "",
-        	qty: "",
-        	cost: "",
-        };
-        var newItem = 1;
+        var items = [];
+       	var newItem = {};
+       	newItem.qty = 1;
 
-        this.addItemToCart = function(items) {
-        	this.items = {
-        		name: "",
-        		qty: "",
-        		cost: "",
-        	};
-        };
+        this.addItemToCart = function() {
+        	items.push({
+            'name': 'tyler',
+            'qty': '10',
+            'cost': '3.50'
+        	});
+        	items.push({
+            'name': 'michael',
+            'qty': '20',
+            'cost': '1.50'
+        	});
 
+        	console.log(items);
+       		console.log(newItem.qty);
+        };	
 
     });
 })();

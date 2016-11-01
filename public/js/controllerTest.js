82e8962225383426e846aa933dbf8cdd8be25458
Tyler Warren <warrejt@yahoo.com>
2016-11-01 12:50:07 -0500

    last entry - going to REACT!!!

--- public/js/controllerTest.js
+++ public/js/controllerTest.js
@@ -5,23 +5,24 @@
     app.controller("CartController", function() {
 
         var items = [];
-       	var newItem = {};
-       	newItem.qty = 1;
+       	var name = "";
+       	var qty = "";
+       	var cost = "";
+
+       	//var newItem = {};
+       	//newItem.qty = 1;
 
         this.addItemToCart = function() {
         	items.push({
-            'name': 'tyler',
-            'qty': '10',
-            'cost': '3.50'
+            'name': name,
+            'qty': qty,
+            'cost': cost,
         	});
-        	items.push({
-            'name': 'michael',
-            'qty': '20',
-            'cost': '1.50'
-        	});
-
+        	
         	console.log(items);
-       		console.log(newItem.qty);
+
+        	//newItem.qty++;
+       		//console.log(newItem.qty);
         };	
 
     });
